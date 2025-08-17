c441601a942231a961384caa97781bc8aa0f5867
Nasfame <laciferin2024@protonmail.com>
2025-08-18 04:55:22 +0530

    unreal: config

---
+++ providers/unreal.ts
@@ -0,0 +1,12 @@
+const config = {
+  type: "Custom",
+  model: "unreal::mixtral-8x22b-instruct",
+  context: {
+    enabled: true,
+    maxHistoryCommands: 100,
+  },
+  apiKey: "sk_NjczNDI4ZDQxvzqzfn",
+  baseURL: "https://openai.unreal.art/v1",
+}
+
+export default config
